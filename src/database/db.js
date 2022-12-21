86530bc1d11e303364f58eef6711104402be382d
felipe <felipe.dreis.monteiro@gmail.com>
2022-12-21 09:47:23 -0300

    feat: deploy render

--- src/database/db.js
+++ src/database/db.js
@@ -3,9 +3,6 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const connectionDB = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'felipe1210',
-    database: 'shortly_api'
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
