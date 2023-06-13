7dd03e5f869c27c465ef07de5753c0e6a0119d99
felipe <felipe.dreis.monteiro@gmail.com>
2023-06-13 11:27:33 -0300

    fix: big int in Post or get user me route

--- src/controllers/getUsersMeController.js
+++ src/controllers/getUsersMeController.js
@@ -1,24 +1,30 @@
 import { prisma } from "../database/db.js";
 
 export default async function(req, res) {
-    try {
-        const token = req.token;
+  try {
+    const token = req.token;
 
-        const promise = await prisma.$queryRaw`
-        SELECT users.id, users.name, SUM(urls.visited) AS "visitCount",
+    const promise = await prisma.$queryRaw`
+      SELECT
+        users.id,
+        users.name,
+        CAST(SUM(urls.visited) AS text) AS "visitCount",
         json_agg(urls.*) AS "shortenedUrls"
-        FROM tokens
+      FROM
+        tokens
         JOIN users ON tokens.id_user = users.id
         JOIN urls ON tokens.id_user = urls.user_id
-        WHERE tokens.token=${token}
-        GROUP BY users.id
-        `;
+      WHERE
+        tokens.token=${token}
+      GROUP BY
+        users.id
+    `;
 
-        const result = JSON.parse(JSON.stringify(promise));
+    const result = JSON.parse(JSON.stringify(promise));
 
-        res.send(result);
-    } catch (err) {
-        console.log(err);
-        res.send(err.message);
-    }
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.send(err.message);
+  }
 }
