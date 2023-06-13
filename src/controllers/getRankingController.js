14379b27744ce710b21273c68307cf7f5d48130c
felipe <felipe.dreis.monteiro@gmail.com>
2023-06-13 10:57:09 -0300

    fix: raw problems

--- src/controllers/getRankingController.js
+++ src/controllers/getRankingController.js
@@ -2,14 +2,14 @@ import { prisma } from "../database/db.js"
 
 export default async function getRankingController(req, res) {
     try {
-        const ranking = await prisma.$queryRaw(`
+        const ranking = await prisma.$queryRaw`
         SELECT users.id, users.name, COUNT(urls) AS "linksCount", SUM(urls.visited) AS "visitCount"
         FROM urls
         JOIN users
         ON urls.user_id = users.id
         GROUP BY users.id, urls.visited
         ORDER BY urls.visited DESC;
-        `);
+        `;
         
         res.send(ranking);
     } catch (err) {
