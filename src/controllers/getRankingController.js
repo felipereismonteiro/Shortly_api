a910108483da1f0e3d286011a89c22884761dfbe
felipe <felipe.dreis.monteiro@gmail.com>
2023-06-13 11:07:09 -0300

    fix: ranking route

--- src/controllers/getRankingController.js
+++ src/controllers/getRankingController.js
@@ -1,19 +1,25 @@
-import { prisma } from "../database/db.js"
+import { prisma } from "../database/db.js";
 
 export default async function getRankingController(req, res) {
-    try {
-        const ranking = await prisma.$queryRaw`
-        SELECT users.id, users.name, COUNT(urls) AS "linksCount", SUM(urls.visited) AS "visitCount"
-        FROM urls
-        JOIN users
-        ON urls.user_id = users.id
-        GROUP BY users.id, urls.visited
-        ORDER BY urls.visited DESC;
-        `;
-        
-        res.send(ranking);
-    } catch (err) {
-        console.log(err);
-        res.send(err.message);
-    }
+  try {
+    const ranking = await prisma.$queryRaw`
+      SELECT users.id, users.name, COUNT(urls) AS "linksCount", SUM(urls.visited) AS "visitCount"
+      FROM urls
+      JOIN users ON urls.user_id = users.id
+      GROUP BY users.id, users.name
+      ORDER BY SUM(urls.visited) DESC;
+    `;
+
+    const serializedRanking = ranking.map(user => ({
+      id: user.id,
+      name: user.name,
+      linksCount: Number(user.linksCount),
+      visitCount: Number(user.visitCount),
+    }));
+
+    res.json(serializedRanking);
+  } catch (err) {
+    console.log(err);
+    res.send(err.message);
+  }
 }
