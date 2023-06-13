d13cde52ecd6ba5aa7ba3df8b42eec0976a21003
felipe <felipe.dreis.monteiro@gmail.com>
2023-06-13 12:00:02 -0300

    fix: all issues

--- src/controllers/openShortUrlController.js
+++ src/controllers/openShortUrlController.js
@@ -3,13 +3,11 @@ import { prisma } from "../database/db.js"
 export default async function openShortUrlController(req, res) {
     try {
         const { shortUrl } = req.params;
-        const promise = await prisma.urls.findFirst({
+        const promise = await prisma.urls.findFirstOrThrow({
             where: {
                 short_url: shortUrl
             }
         })
-        
-        if (!promise) return res.sendStatus(404);
 
         let visitedNumber = promise.visited += 1;
 
@@ -17,9 +15,13 @@ export default async function openShortUrlController(req, res) {
             data: {
                 short_url: shortUrl,
                 visited: visitedNumber
+            },
+            where: {
+                id: promise.id
             }
         })
-        res.redirect(promise);
+        console.log(promise)
+        res.redirect(promise.url);
     } catch(err) {
         console.log(err);
         res.send(err.message);
